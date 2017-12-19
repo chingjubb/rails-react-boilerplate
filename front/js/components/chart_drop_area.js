9b2670998398f95f66285a32b99e4bbf8e4c6380
Chingju Hu <chingju@periscopedata.com>
2017-12-18 17:30:31 -0800

    Add drag and drop to the area

--- front/js/components/chart_drop_area.js
+++ front/js/components/chart_drop_area.js
@@ -4,34 +4,65 @@
 
 import React from 'react';
 
-import { Button, Grid, Col, Row, Panel, Table } from 'react-bootstrap';
+import { Button, Grid, Col, Row, Panel, Table, Alert, Well } from 'react-bootstrap';
 export default class ChartDropArea extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dragOver1: false,
+      dragOver2: false,
     };
+    this.onDropList1 = this.onDropList1.bind(this);
+    this.onDropList2 = this.onDropList2.bind(this);
+    this.onDragOverList1 = this.onDragOverList1.bind(this);
+    this.onDragOverList2 = this.onDragOverList2.bind(this);
+  }
+  onDropList1(ev) {
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    var value = ev.dataTransfer.getData("text/plain");
+    console.log("on Drop value: ", value);
+    this.props.addToList1(value);
+  }
+  onDragOverList1(ev) {
+    ev.preventDefault();
+    this.setState({dragOver1: true});
+  }
+  onDropList2(ev) {
+    ev.preventDefault();
+    // Get the id of the target and add the moved element to the target's DOM
+    var value = ev.dataTransfer.getData("text/plain");
+    console.log("on Drop value: ", value);
+    this.props.addToList2(value);
+    this.setState({dragOver2: true});
+  }
+  onDragOverList2(ev) {
+    ev.preventDefault();
   }
   render() {
+
     return (
       <div style={{position:'relative'}}>
-        <div style={{border:'1px solid gray',backgroundColor:'gray', width:'550',height:'3px', position:'absolute',top:'50px'}}></div>
+        <div style={{border:'1px solid gray',backgroundColor:'gray', width:'550px',height:'3px', position:'absolute',top:'50px'}}></div>
         <div style={{border:'1px solid gray',backgroundColor:'gray', width:'3px',height:'250px', position:'absolute',left:'130px'}}></div>
      
-        <div id='Column Drop area' style={{border:'1px dotted gray',
-                      backgroundColor:'white', 
-                      width:'450',
-                      height:'50px', 
-                      position:'absolute',
-                      left:'130px',
-                      padding:'10px'}}>Drop field here</div>
+        <div id='Column Drop area' style={{
+                width:'450px',
+                height:'50px', 
+                position:'absolute',
+                left:'133px',
+                padding:'10px',
+                paddingLeft:'30px'}}
+                onDrop={this.onDropList1} onDragOver={this.onDragOverList1}>Drop field here</div>
 
-        <div id='Row Drop area' style={{border:'1px dotted gray',
-                      backgroundColor:'white', 
+        <div id='Row Drop area' style={{ 
                       width:'130px',
                       height:'200px', 
                       position:'absolute',
                       top:'50px',
-                      padding:'10px'}}>Drop field here</div>
+                      padding:'10px',
+                      paddingTop:'30px',}}
+                      onDrop={this.onDropList2} onDragOver={this.onDragOverList2}>Drop field here</div>
       </div>
     );
   }
